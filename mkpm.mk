3655a5cb2f7d3908646ea3d86cc89e0ff312a6bf
Jam Risser <jam@codejam.ninja>
2021-10-28 02:54:42 -0500

    Added mkpm support

---
+++ mkpm.mk
@@ -0,0 +1,47 @@
+# File: /mkpm.mk
+# Project: multiverse-example
+# File Created: 27-10-2021 16:42:55
+# Author: Jam Risser
+# -----
+# Last Modified: 28-10-2021 00:25:31
+# Modified By: Jam Risser
+# -----
+# BitSpur Inc (c) Copyright 2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+MKPM_PACKAGES := \
+	docker=0.0.3 \
+	gnu=0.0.2 \
+	mkchain=0.0.2
+
+MKPM_REPOS := \
+	https://gitlab.com/bitspur/community/mkpm-stable.git
+
+############# MKPM BOOTSTRAP SCRIPT BEGIN #############
+MKPM_BOOTSTRAP := https://bitspur.gitlab.io/community/mkpm/bootstrap.mk
+NULL := /dev/null
+TRUE := true
+ifeq ($(OS),Windows_NT)
+	NULL = nul
+	TRUE = type nul
+endif
+-include .mkpm/.bootstrap.mk
+.mkpm/.bootstrap.mk:
+	@mkdir .mkpm 2>$(NULL) || $(TRUE)
+	@cd .mkpm && \
+		$(shell curl --version >$(NULL) 2>$(NULL) && \
+			echo curl -L -o || \
+			echo wget --content-on-error -O) \
+		.bootstrap.mk $(MKPM_BOOTSTRAP) >$(NULL)
+############## MKPM BOOTSTRAP SCRIPT END ##############
