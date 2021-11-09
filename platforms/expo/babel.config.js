a0d58f3df16c94b364e13bd824a7f5f9d204aee7
Clay Risser <clayrisser@gmail.com>
2021-11-09 05:45:07 -0600

    Refactored for multirepo

---
+++ platforms/expo/babel.config.js
@@ -0,0 +1,12 @@
+// module.exports = { presets: ['@expo/next-adapter/babel'] };
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }]
+    ]
+  };
+};
