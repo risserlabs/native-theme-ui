7024a0b7da1bee913c24a7b5d1a7d275cce97439
Clay Risser <clayrisser@gmail.com>
2021-11-17 09:11:20 -0600

    Added initial addons

--- src/.storybook/main.ts
+++ src/.storybook/main.ts
@@ -4,10 +4,23 @@ const config: StorybookConfig = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx|md|mdx)'],
   logLevel: 'debug',
   addons: [
+    '@etchteam/storybook-addon-status',
+    '@luigiminardim/storybook-addon-globals-controls',
+    '@pbutlewski/storybook-html',
+    '@react-theming/storybook-addon',
+    '@storybook/addon-a11y',
     '@storybook/addon-essentials',
-    '@storybook/addon-storysource',
+    '@storybook/addon-ie11',
+    '@storybook/addon-links',
     '@storybook/addon-storyshots',
-    '@storybook/addon-a11y'
+    '@storybook/addon-storysource',
+    'addon-screen-reader',
+    'storybook-addon-breakpoints',
+    'storybook-addon-grid',
+    'storybook-addon-paddings',
+    // 'storybook-addon-themes',
+    'storybook-color-picker',
+    'storybook-mobile'
   ],
   typescript: {
     check: true,
--- src/.storybook/preview.js
+++ src/.storybook/preview.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import { ThemeProvider, convert, themes } from '@storybook/theming';
+import { ThemeProvider, themes, convert } from '@storybook/theming';
+import { addDecorator } from '@storybook/react';
+import { withDesign } from 'storybook-addon-designs';
+import { withGlobals } from '@luigiminardim/storybook-addon-globals-controls';
+import { withThemes } from '@react-theming/storybook-addon';
+import { createDecorator, setParameters } from '@storybook/addon-devkit';
 
-export const parameters = {};
+themes.light.name = themes.light.base;
+themes.dark.name = themes.dark.base;
 
-export const decorators = [
-  (StoryFn) => (
-    <ThemeProvider theme={convert(themes.light)}>
-      <StoryFn />
-    </ThemeProvider>
-  )
-];
+export const parameters = {
+  // themes: {
+  //   default: 'twitter',
+  //   list: [
+  //     { name: 'twitter', class: 'theme-twt', color: '#00aced' },
+  //     { name: 'facebook', class: 'theme-fb', color: '#3b5998' }
+  //   ]
+  // },
+  status: {
+    statuses: {
+      released: {
+        background: '#0000ff',
+        color: '#ffffff',
+        description: 'This component is stable and released'
+      }
+    }
+  },
+  globalsControls: {}
+};
 
 export const loaders = [];
+
+const themingDecorator = withThemes(null, [themes.light, themes.dark], {
+  providerFn: ({ theme, children }) => (
+    <ThemeProvider theme={convert(theme)}>{children}</ThemeProvider>
+  )
+});
+const withDisplayGlobals = withGlobals((Story, _globalValues) => <Story />);
+
+// addDecorator(themingDecorator);
+addDecorator(withDesign);
+addDecorator(withDisplayGlobals);
+
+export const globalTypes = {
+  boolean: {
+    name: 'Boolean',
+    description: 'Boolean Description',
+    defaultValue: true,
+    control: { type: 'boolean' }
+  }
+};
