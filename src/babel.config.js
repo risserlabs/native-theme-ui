eb3271c7afbebe11b38b00dc0396b875226e50cb
Clay Risser <clayrisser@gmail.com>
2021-11-19 10:54:15 -0600

    started ci

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,6 +1,16 @@
 module.exports = (api) => {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      'babel-plugin-macros',
+      'babel-plugin-transform-typescript-metadata',
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-transform-runtime'
+    ]
   };
 };
