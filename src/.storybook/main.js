164749fa3043a326ca0324f6f1dc3e403c5995c1
Clay Risser <clayrisser@gmail.com>
2021-11-19 03:42:10 -0600

    Fixed storybook compile bugs

---
+++ src/.storybook/main.js
@@ -0,0 +1,114 @@
+const babelConfig = {
+  presets: [],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+  ]
+};
+
+const config = {
+  stories: ['../**/*.stories.@(js|jsx|ts|tsx|md|mdx)'],
+  logLevel: 'debug',
+  addons: [
+    '@etchteam/storybook-addon-status',
+    '@luigiminardim/storybook-addon-globals-controls',
+    '@pbutlewski/storybook-html',
+    '@storybook/addon-a11y',
+    '@storybook/addon-ie11',
+    '@storybook/addon-links',
+    '@storybook/addon-storyshots',
+    '@storybook/addon-storysource',
+    'addon-screen-reader',
+    'storybook-addon-breakpoints',
+    'storybook-addon-grid',
+    'storybook-addon-paddings',
+    'storybook-color-picker',
+    'storybook-mobile',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: true,
+        backgrounds: true,
+        controls: true,
+        docs: true,
+        toolbars: true,
+        viewport: true
+      }
+    }
+  ],
+  typescript: {
+    check: true,
+    checkOptions: {},
+    reactDocgenTypescriptOptions: {}
+  },
+  core: {
+    builder: 'webpack5'
+  },
+  features: {
+    postcss: false,
+    buildStoriesJson: true
+  },
+  webpackFinal: async (config, {}) => {
+    patchBabel(config);
+    return {
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          'react-native$': 'react-native-web'
+        }
+      },
+      ...config
+    };
+  }
+};
+
+function patchBabel(config) {
+  config.module.rules.forEach((rule) => {
+    if (Array.isArray(rule.use)) {
+      rule.use.forEach((item) => {
+        if (item.loader?.indexOf('babel-loader') > -1) {
+          const plugins = [...(babelConfig.plugins || [])];
+          const presets = [...(babelConfig.presets || [])];
+          const pluginsSet = new Set(
+            plugins.map((plugin) =>
+              require.resolve(Array.isArray(plugin) ? plugin[0] : plugin)
+            )
+          );
+          const presetsSet = new Set(
+            presets.map((preset) =>
+              require.resolve(Array.isArray(preset) ? preset[0] : preset)
+            )
+          );
+          (item.options.plugins || []).forEach((plugin) => {
+            if (
+              !pluginsSet.has(
+                require.resolve(Array.isArray(plugin) ? plugin[0] : plugin)
+              )
+            ) {
+              plugins.push(plugin);
+            }
+          });
+          (item.options.presets || []).forEach((preset) => {
+            if (
+              !presetsSet.has(
+                require.resolve(Array.isArray(preset) ? preset[0] : preset)
+              )
+            ) {
+              presets.push(preset);
+            }
+          });
+          item.options = {
+            ...item.options,
+            ...babelConfig,
+            plugins,
+            presets
+          };
+        }
+      });
+    }
+  });
+}
+
+module.exports = config;
--- src/.storybook/preview.js
+++ src/.storybook/preview.js
@@ -2,20 +2,10 @@ import { ThemeProvider, themes, convert } from '@storybook/theming';
 import { addDecorator } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
 import { withGlobals } from '@luigiminardim/storybook-addon-globals-controls';
-import { withThemes } from '@react-theming/storybook-addon';
-import { createDecorator, setParameters } from '@storybook/addon-devkit';
-
-themes.light.name = themes.light.base;
-themes.dark.name = themes.dark.base;
 
 export const parameters = {
-  // themes: {
-  //   default: 'twitter',
-  //   list: [
-  //     { name: 'twitter', class: 'theme-twt', color: '#00aced' },
-  //     { name: 'facebook', class: 'theme-fb', color: '#3b5998' }
-  //   ]
-  // },
+  facelift: {},
+  globalsControls: {},
   status: {
     statuses: {
       released: {
@@ -24,20 +14,13 @@ export const parameters = {
         description: 'This component is stable and released'
       }
     }
-  },
-  globalsControls: {}
+  }
 };
 
 export const loaders = [];
 
-const themingDecorator = withThemes(null, [themes.light, themes.dark], {
-  providerFn: ({ theme, children }) => (
-    <ThemeProvider theme={convert(theme)}>{children}</ThemeProvider>
-  )
-});
 const withDisplayGlobals = withGlobals((Story, _globalValues) => <Story />);
 
-// addDecorator(themingDecorator);
 addDecorator(withDesign);
 addDecorator(withDisplayGlobals);
 
