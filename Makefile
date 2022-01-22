49cb2bc2ce7c3199708e5d958d9183f14d238bb1
Clay Risser <clayrisser@gmail.com>
2022-01-22 09:06:02 -0600

    Updated mkpm

--- Makefile
+++ Makefile
@@ -1,10 +1,32 @@
+# File: /Makefile
+# Project: multiverse
+# File Created: 04-12-2021 07:22:50
+# Author: Clay Risser <email@clayrisser.com>
+# -----
+# Last Modified: 22-01-2022 09:04:56
+# Modified By: Clay Risser <email@clayrisser.com>
+# -----
+# Risser Labs LLC (c) Copyright 2021 - 2022
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 MONOREPO := 1
 
 include mkpm.mk
 ifneq (,$(MKPM_READY))
 include $(MKPM)/gnu
 include $(MKPM)/mkchain
-include $(PROJECT_ROOT)/yarn.mk
+include $(MKPM)/yarn
 include config.mk
 
 ACTIONS += install ##
--- mkpm.mk
+++ mkpm.mk
@@ -1,25 +1,50 @@
+# File: /mkpm.mk
+# Project: multiverse
+# File Created: 04-12-2021 02:19:00
+# Author: Clay Risser <email@clayrisser.com>
+# -----
+# Last Modified: 22-01-2022 09:05:01
+# Modified By: Clay Risser <email@clayrisser.com>
+# -----
+# Risser Labs LLC (c) Copyright 2021 - 2022
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 MKPM_PACKAGES := \
-	docker=0.0.8 \
+	docker=0.0.9 \
+	dotenv=0.0.4 \
+	envcache=0.0.2 \
 	gnu=0.0.3 \
-	mkchain=0.0.13
+	mkchain=0.0.15 \
+	yarn=0.0.1
 
 MKPM_REPOS := \
-	https://gitlab.com/bitspur/community/mkpm-stable.git
+	https://gitlab.com/risserlabs/community/mkpm-stable.git
 
 ############# MKPM BOOTSTRAP SCRIPT BEGIN #############
-MKPM_BOOTSTRAP := https://bitspur.gitlab.io/community/mkpm/bootstrap.mk
+MKPM_BOOTSTRAP := https://risserlabs.gitlab.io/community/mkpm/bootstrap.mk
+export PROJECT_ROOT := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 NULL := /dev/null
 TRUE := true
 ifneq ($(patsubst %.exe,%,$(SHELL)),$(SHELL))
 	NULL = nul
 	TRUE = type nul
 endif
--include .mkpm/.bootstrap.mk
-.mkpm/.bootstrap.mk:
+-include $(PROJECT_ROOT)/.mkpm/.bootstrap.mk
+$(PROJECT_ROOT)/.mkpm/.bootstrap.mk:
 	@mkdir $(@D) 2>$(NULL) || $(TRUE)
-	@cd $(@D) && \
-		$(shell curl --version >$(NULL) 2>$(NULL) && \
+	@$(shell curl --version >$(NULL) 2>$(NULL) && \
 			echo curl -L -o || \
 			echo wget --content-on-error -O) \
-		$(@F) $(MKPM_BOOTSTRAP) >$(NULL)
+		$@ $(MKPM_BOOTSTRAP) >$(NULL)
 ############## MKPM BOOTSTRAP SCRIPT END ##############
