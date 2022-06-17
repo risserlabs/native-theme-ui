c05fc7ce365d4b6cf3b35bd516a53050ebe7e9d4
Clay Risser <clayrisser@gmail.com>
2022-06-17 08:21:57 -0500

    Added button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,41 @@
+/**
+ * File: /components/Button/index.tsx
+ * Project: -
+ * File Created: 17-06-2022 07:34:18
+ * Author: Clay Risser
+ * -----
+ * Last Modified: 17-06-2022 08:05:05
+ * Modified By: Clay Risser
+ * -----
+ * Risser Labs LLC (c) Copyright 2021 - 2022
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { FC } from 'react';
+import { AutoContrast } from '@risserlabs/auto-contrast';
+// import { Pressable as RNPressable } from 'react-native';
+import { Pressable as DPressable, SxProp } from 'dripsy';
+
+type ButtonProps = {
+  autoContrast?: AutoContrast;
+  sx?: SxProp;
+};
+
+const Button: FC<ButtonProps> = (props: ButtonProps) => {
+  return <DPressable sx={props.sx}>Hello</DPressable>;
+};
+
+Button.defaultProps = {};
+
+export default Button;
