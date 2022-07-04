3de08d322a941410b74349b7b0c916aa9ba39838
Clay Risser <clayrisser@gmail.com>
2022-07-04 11:25:05 -0500

    Fixed type errors

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,7 +4,7 @@
  * File Created: 30-06-2022 08:40:58
  * Author: Clay Risser
  * -----
- * Last Modified: 03-07-2022 10:22:14
+ * Last Modified: 04-07-2022 11:24:51
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2021 - 2022
@@ -22,27 +22,30 @@
  * limitations under the License.
  */
 
+export type { DripsyFinalTheme as Theme } from "@dripsy/core";
 import modulesToTranspile from "./modulesToTranspile.json";
 
-export { useDripsyTheme as useTheme } from "@dripsy/core/src/use-dripsy-theme";
 export * from "./components";
 export * from "./hooks";
-export * from "@dripsy/core/src/css/create-themed-component";
+
+export {
+  BreakpointIndexProvider,
+  ContainerQuery,
+  Styles,
+  createThemedComponent,
+  css,
+  getBreakpointIndex,
+  remToPixels,
+  styled,
+  useBreakpointIndex,
+  useBreakpoints,
+  useDripsyTheme as useTheme,
+  useResponsiveValue,
+  useSx,
+} from "@dripsy/core";
+
 export * from "@dripsy/core/src/provider";
 export * from "@dripsy/core/src/css/scales";
-export { useResponsiveValue } from "@dripsy/core/src/css/use-responsive-value";
-export { Styles, css } from "@dripsy/core/src/css";
-export type { DripsyFinalTheme as Theme } from "@dripsy/core/src/declarations";
-export { styled } from "@dripsy/core/src/css/styled";
-export { useBreakpointIndex } from "@dripsy/core/src/css/breakpoint-context";
-export { getBreakpointIndex } from "@dripsy/core/src/css/get-breakpoint-index";
-export {
-  ContainerQuery,
-  BreakpointIndexProvider,
-} from "@dripsy/core/src/container-query";
-export { useBreakpoints } from "@dripsy/core/src/css/breakpoints";
-export { remToPixels } from "@dripsy/core/src/utils/rem-to-pts";
-export { useSx } from "@dripsy/core/src/use-sx";
 export * from "@dripsy/core/src/declarations";
 export * from "@dripsy/core/src/css/types";
 
